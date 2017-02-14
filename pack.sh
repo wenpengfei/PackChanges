0fc5eb84d5451df7dc81628e3c4d9e6d636d2b94
catwen <jquery.min.js@gmail.com>
2017-02-14 15:47:29 +0800

    Create pack.sh

---
+++ pack.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function updateTimeSpan(){
+	echo 'start updateTimeSpan...'
+	  for arg in $*
+		do
+		  echo '====='
+		  echo $arg
+		  echo '====='
+	  done
+	echo 'updateTimeSpan complete' 
+}
+
+function findChangedCSSAndScript(){
+  updateTimeSpan $(git diff origin/Develop --name-only -- '*.js' '*.css')
+}
+
+function zipAllChanges(){
+ echo 'start zip...'
+ echo 'done' 
+}
+
+findChangedCSSAndScript
+zipAllChanges
+
