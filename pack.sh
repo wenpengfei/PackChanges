9fdc8fa0a8b6aeac5ec2e60dad089bacbe02fe8b
wpf <jquery.min.js@gmail.com>
2017-02-16 11:58:53 +0800

    feat: Add color

--- pack.sh
+++ pack.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 function updateTimeSpan(){
 if [ -n "$1" ] ;then
-  echo "\033[32mstart updateTimeSpan...\033[0m"
+  echo -e "\033[32mstart updateTimeSpan...\033[0m"
     for arg in $*
     do
       echo "processing: "$arg
       sed -i '' "s#${arg}#df#g" `grep * -rl /Users/catwen/Documents/github/PackChanges`
     done
-  echo "\033[32mupdateTimeSpan complete.\033[0m"
+  echo -e "\033[32mupdateTimeSpan complete.\033[0m"
   #git add . 
   #git commit -m 'update timespan(auto generate)'
 else
-  echo "\033[33mthis repo's js & css has nothing changed, pass updateTimeSpan\033[0m"
+  echo -e "\033[33mthis repo's js & css has nothing changed, pass updateTimeSpan\033[0m"
 fi
 }
 
@@ -20,9 +20,9 @@ function findChangedCSSAndScript(){
 }
 
 function zipAllChanges(){
-  echo "\033[32mstart zip...\033[0m"
-  git diff origin/Develop --name-only | xargs zip `date '+%Y%m%d%H%M%S'.zip`
-  echo "\033[32mdone!\033[0m"
+  echo -e "\033[32mstart zip...\033[0m"
+  git diff master origin/Develop --name-only | xargs zip `date '+%Y%m%d%H%M%S'.zip`
+  echo -e "\033[32mdone!\033[0m"
 }
 
 findChangedCSSAndScript
